cd221e37a29d91583c7d77d271ca062a3e8256cd
Jon-Paul <Jon-Paul@161.31.72.167>
2018-04-17 16:50:28 -0500

    2nd Commit

--- sfweng/client/MyClass.java
+++ sfweng/client/MyClass.java
@@ -6,11 +6,11 @@ public class MyClass
   public static void main(String[] args)
   {
     //Print statements
-    System.out.println("Hello Joe");
+    System.out.println("Hello Bob");
     
-    System.out.println("Another print statement 2");
+    System.out.println("Another print statement");
     
-    System.out.println("3rd Print statement");
+    System.out.println("3rd statement");
 
   }
 
